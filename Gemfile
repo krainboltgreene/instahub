49db7eaf53f0fee6330ae9148dd939f4edfd1a8e
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-12-02 11:22:50 -0600

    Adding views, layout modifications, gemfile additions.

--- Gemfile
+++ Gemfile
@@ -3,8 +3,9 @@ source :rubygems
 gem 'sinatra'
 gem 'rack'
 gem 'heroku'
-
+gem 'thin'
 gem 'redcarpet'
 gem 'slim'
 gem 'sass'
 gem 'ripple'
+gem 'curb'
--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'bundler/setup'
 Bundler.require
 
+configure do
+  set :markdown, options: {fenced_code_blocks: true}
+end
+
 get '/' do
   slim :index
 end
 
-get '/styles/main.css' do
-  sass :main
+get '/hub/:user/:repo?' do |user, repo|
+  raw = "https://raw.github.com/"
+  url = raw + File.join(user, repo, "master", "README.markdown")
+  @body = Curl::Easy.perform(url).body_str
+  slim :readme
 end
