9295f788c032de8db9bbf060fe9eb17da167890d
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-12-07 01:19:48 -0600

    Adding some more style changes, images.

--- app.rb
+++ app.rb
@@ -8,25 +8,26 @@ get '/' do
   slim :index
 end
 
-get '/i/:user/:repo?' do |user, repo|
-  github = github_request using_slug_from user, repo
-  travis = travis_request using_slug_from user, repo
+get '/i/:user/:repo?' do |user, repository|
+  slug = make_slug_with user, repository
+  github = github_request_with slug
+  travis = travis_request_with slug
   readme = readmeify github
   license = licensify readme
   build = apify travis
-  slim :readme, locals: { readme: readme, build: build, license: license }
+  slim :readme, locals: { slug: slug, readme: readme, build: build, license: license }
 end
 
-def using_slug_from(user, repository)
+def make_slug_with(user, repository)
   user + '/' + repository
 end
 
-def github_request(slug)
+def github_request_with(slug)
   response = Curl::Easy.perform(GITHUB_URL + slug).body_str
   if response.include? "404" then response = '<div id="readme"><div class="wikistyle"><p>Repository Not Found</p></div></div>' else response end
 end
 
-def travis_request(slug)
+def travis_request_with(slug)
   response = Curl::Easy.perform(TRAVIS_URL + slug + '.json').body_str
   if response.include? "404" then response = "{}" else response end
 end
